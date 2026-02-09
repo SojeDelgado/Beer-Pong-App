f20e95f60f3a1b8ccfc576756fe3ab7f4bd7f992
SojeDelgado <delgadocoreeajosemiguel@gmail.com>
2026-02-09 16:58:00 -0600

    Refactorizacion en el componente de MatchesList.     - Anteriormente se suscribia en el servicio (lo cual es mala practica),     ahora utiliza rxResource (se suscribe y desuscribe automaticamente) para implementar     la paginación adecuadamente con signals.

--- src/app/matches/matches-list/matches-list.ts
+++ src/app/matches/matches-list/matches-list.ts
@@ -1,7 +1,11 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
+// Angular
+import { Component, computed, inject, signal, WritableSignal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+// RxJS
+import { rxResource } from '@angular/core/rxjs-interop';
+// Matches
 import { MatchesService } from '../matches.service';
 import { MatchComponent } from "./match/match.component";
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-matches-list',
@@ -12,21 +16,42 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class MatchesList {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private matchesService = inject(MatchesService);
 
-  matchesService = inject(MatchesService);
-  matches = this.matchesService.matches;
-  pagination = this.matchesService.pagination;
-
-  currentPage = signal(0);
+  currentPage = signal(1);
   limit = signal(10);
-  dateFilter = signal('Reciente');
+  dateFilter = signal('Recientes');
+
+  // loader was renamed to stream in rxResource in v20 (rxResource).
+  // also request was renamed to params:
+  // link to resource: https://www.youtube.com/watch?v=_KyCmpMlVTc&list=LL&index=2
+
+  readonly matchesResponse = rxResource({
+    params: () => ({
+      page: this.currentPage(),
+      limit: this.limit(),
+      dateFilter: this.dateFilter()
+    }),
+    stream: ({ params }) => this.matchesService.loadMatches(
+      params.page, params.limit, params.dateFilter,
+    ),
+    defaultValue: {
+      data: [], meta: {
+        total: 0,
+        page: 1,
+        lastPage: 1
+      }
+    }
+  });
+
+  readonly matches = computed(() => this.matchesResponse.value().data);
+  pagination = computed(() => this.matchesResponse.value().meta);
 
   isDropdownLimitOpen = signal(false);
   isDropdownOpen = signal(false);
 
-  // Puedo ver la documentacion en:
+  // Query parameters docs:
   // https://v20.angular.dev/guide/routing/read-route-state#query-parameters
-
   constructor() {
     this.route.queryParams.subscribe(params => {
       const page = Number(params['page']) || 1;
@@ -41,28 +66,32 @@ export class MatchesList {
 
   prevPage() {
     if (this.currentPage() > 1) {
+      this.currentPage.update(page => page - 1);
       this.router.navigate([], {
-        queryParams: { page: this.currentPage() - 1 },
+        queryParams: { page: this.currentPage() },
         queryParamsHandling: 'merge' // Preserve other query parameters
       })
     }
   }
 
   nextPage() {
+    this.currentPage.update(page => page + 1);
     this.router.navigate([], {
-      queryParams: { page: this.currentPage() + 1 },
+      queryParams: { page: this.currentPage() },
       queryParamsHandling: 'merge' // Preserve other query parameters
     })
   }
 
   goToPage(page: number) {
+    this.currentPage.update(() => page);
     this.router.navigate([], {
-      queryParams: { page },
-      queryParamsHandling: 'merge'
-    });
+      queryParams: { page: this.currentPage() },
+      queryParamsHandling: 'merge' // Preserve other query parameters
+    })
   }
 
   changeLimit(limit: number) {
+    this.limit.update(() => limit)
     this.router.navigate([], {
       queryParams: { page: 1, limit },
       queryParamsHandling: 'merge'
@@ -70,6 +99,7 @@ export class MatchesList {
   }
 
   filter(dateFilter: string) {
+    this.dateFilter.update(() => dateFilter);
     this.router.navigate([], {
       queryParams: { dateFilter },
       queryParamsHandling: 'merge'
